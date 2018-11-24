40e1a1560820ccabf0d0bd408e8e2a0c4fd4844f
Jakob Homan <jhoman@lyft.com>
2018-11-23 16:01:06 -0800

    Add ray.hpp

---
+++ cpp/ray.hpp
@@ -0,0 +1,18 @@
+#ifndef RAYH
+#define RAYH
+
+#include "vec3.hpp"
+
+class ray {
+  public:
+    ray() {}
+    ray(const vec3& a, const vec3& b) { A = a; B = b; }
+    vec3 origin() const { return A; }
+    vec3 direction() const { return B; }
+    vec3 point_at_parameter(float t) const { return A + B * t; }
+
+    vec3 A;
+    vec3 B;
+};
+
+#endif // RAYH
