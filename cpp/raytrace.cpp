d678e9cbb9a2de5f5017d828ab14dd33d7c68e45
Jakob Homan <jhoman@lyft.com>
2018-11-26 11:50:27 -0800

    Add surface normal lighting.

--- cpp/raytrace.cpp
+++ cpp/raytrace.cpp
@@ -2,22 +2,34 @@
 #include "vec3.hpp"
 #include "ray.hpp"
 
+bool hit_sphere(const vec3& center, float radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    float a = dot(r.direction(), r.direction());
+    float b = 2.0 * dot(oc, r.direction());
+    float c = dot(oc, oc) - radius * radius;
+    float discriminant = b * b - 4 * a * c;
+    return (discriminant > 0);
+}
+
 vec3 color(const ray& r) {
+    if(hit_sphere(vec3(0, 0, -1), 0.5, r)) {
+        return vec3(1, 0, 0);
+    }
+
     vec3 unit_direction = unit_vector(r.direction());
     float t = 0.5 * (unit_direction.y() + 1.0);
 
     return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
 
-
 int main() {
-    int nx = 2880 / 4;
-    int ny = 1800 / 4;
+    int nx = 2880 / 4; //200; //2880 / 4;
+    int ny = 1800 / 4; //1800 / 4;
 
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
     vec3 lower_left_corner(-2.0, -1.0, -1.0);
-    vec3 horizontal(0.0, 2.0, 0.0);
+    vec3 horizontal(4.0, 0.0, 0.0);
     vec3 vertical(0.0, 2.0, 0.0);
     vec3 origin(0.0, 0.0, 0.0);
 
